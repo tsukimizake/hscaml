e443615b1ec3e8bd2093deb423bf3f5148c1cddb
shomasd <shomasd@ueda.info.waseda.ac.jp>
2018-03-27 16:11:02 +0900

    impl of SetTypeConstraints

--- typechecker/SetTypeConstraints.hs
+++ typechecker/SetTypeConstraints.hs
@@ -6,11 +6,13 @@ import Data.Set as S
 import TypeCheckUtil
 import Control.Lens.Operators
 import Control.Lens as L
-import Unify
+import Debug.Trace
 
 setTypeConstraints :: S.Set TypeConstraint -> TExpr -> TExpr
 setTypeConstraints cs te = S.foldr impl te cs
 
 
 impl :: TypeConstraint -> TExpr -> TExpr
-impl cste te = undefined
+impl (TypeEq (TypeVar ls) r) te = replaceTypeVar (TV ls) r te
+impl (TypeEq l (TypeVar rs)) te = replaceTypeVar (TV rs) l te
+impl _ te = te
