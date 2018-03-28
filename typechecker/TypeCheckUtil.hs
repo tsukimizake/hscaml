0b9afe725d209e077726ed2e20c98b0f6a71a2d9
shomasd <shomasd@ueda.info.waseda.ac.jp>
2018-03-28 21:47:53 +0900

    fix inf recursion of RenameSymsByScope (very dirty)

--- typechecker/TypeCheckUtil.hs
+++ typechecker/TypeCheckUtil.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS -Wall #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE Strict #-}
 module TypeCheckUtil where
 import Types
 import Data.Text
@@ -13,50 +14,50 @@ traverseExpr f x@(Constant _) = f x
 traverseExpr f x@(Var _) = f x
 traverseExpr f (Paren e) = do
     e'  <- traverseExpr f e
-    f $ Paren e'
+    pure $ Paren e'
 traverseExpr f (InfixOpExpr e iop g) = do
     e'  <- traverseExpr f e
     g'  <- traverseExpr f g
-    f $ InfixOpExpr e' iop g'
+    pure $ InfixOpExpr e' iop g'
 traverseExpr f (BegEnd e) = do
     e'  <- traverseExpr f e
-    f $ BegEnd e'
+    pure $ BegEnd e'
 traverseExpr f (MultiExpr e) = do
     e' <- mapM f e
-    f $ MultiExpr e'
+    pure $ MultiExpr e'
 traverseExpr f (Constr e) = do
     e'  <- traverseExpr f e
-    f $ Constr e'
+    pure $ Constr e'
 traverseExpr f (IfThenElse e1 e2 e3) = do
     e1'  <- traverseExpr f e1
     e2'  <- traverseExpr f e2
     e3'  <- traverseExpr f e3
-    f $ IfThenElse e1' e2' e3'
+    pure $ IfThenElse e1' e2' e3'
 traverseExpr f (Match e1 e2) = do
     e1'  <- traverseExpr f e1
     e2' <- mapM (\(x, y) -> do
                      y'  <- traverseExpr f y
                      pure (x, y')) e2
-    f $ Match e1' e2'
+    pure $ Match e1' e2'
 traverseExpr f (While e1 e2) = do
     e1'  <- traverseExpr f e1
     e2'  <- traverseExpr f e2
-    f $ While e1' e2'
+    pure $ While e1' e2'
 traverseExpr f (FunApply e1 e2) = do
     e1'  <- traverseExpr f e1
     e2' <- mapM f e2
-    f $ FunApply e1' e2'
+    pure $ FunApply e1' e2'
 traverseExpr f (Let e1 e2) =  do
     e2'  <- traverseExpr f e2
-    f $ Let e1 e2'
+    pure $ Let e1 e2'
 traverseExpr f (LetRec e1 e2) =  do
     e2'  <- traverseExpr f e2
-    f $ LetRec e1 e2'
+    pure $ LetRec e1 e2'
 traverseExpr f (LetIn e1 e2 e3) = do
     e2'  <- traverseExpr f e2
     e3'  <- traverseExpr f e3
-    f $ LetIn e1 e2' e3'
-traverseExpr _ (TypeDecl e1 e2) = pure $ TypeDecl e1 e2
+    pure $ LetIn e1 e2' e3'
+traverseExpr f (TypeDecl e1 e2) = f $ TypeDecl e1 e2
 
 traverseTExpr :: (Monad m) => (TExpr -> m TExpr) -> TExpr -> m TExpr
 traverseTExpr f x@(TConstant _ _) = f x
